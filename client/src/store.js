c074a41b3fc801a6e98dedd05ba643e8f19569b4
kar-cloud <karanmann1545@gmail.com>
2022-07-02 12:26:14 +0530

    changes to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,19 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import auth from "./redux/reducers/state";
-import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 const middleware =
   process.env.NODE_ENV !== "production"
     ? [require("redux-immutable-state-invariant").default(), thunk]
     : [thunk];
-// const middleware = [thunk];
 
-const store = createStore(
-  auth,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+const store = createStore(auth, initialState, applyMiddleware(...middleware));
 
 export default store;
